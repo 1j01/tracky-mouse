da971946152d34ad715374021047e9f4b82fecce
Isaiah Odhner <isaiahodhner@gmail.com>
2021-05-15 21:41:04 -0400

    Allow only a single instance of the application

--- tracky-mouse-electron/src/electron-main.js
+++ tracky-mouse-electron/src/electron-main.js
@@ -65,6 +65,21 @@ app.on('ready', () => {
 
 });
 
+// Prevent multiple instances of the app
+if (!app.requestSingleInstanceLock()) {
+	app.quit();
+}
+
+app.on('second-instance', () => {
+	if (mainWindow) {
+		if (mainWindow.isMinimized()) {
+			mainWindow.restore();
+		}
+
+		mainWindow.show();
+	}
+});
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
