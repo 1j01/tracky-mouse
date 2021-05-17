1db60f495e5420949a457039c1b520e7fda2a8b7
Isaiah Odhner <isaiahodhner@gmail.com>
2021-05-17 18:29:08 -0400

    setup postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
 import { terser } from "rollup-plugin-terser";
-import scss from 'rollup-plugin-scss'
+import postcss from 'rollup-plugin-postcss'
 import pkg from './package.json';
 
 export default {
 	input: 'tracky-mouse.js',
 	plugins: [
 		terser(),
-		scss({
-			output: 'dist/tracky-mouse.min.css',
-			outputStyle: "compressed",
-		}),
+		postcss(),
 	],
 	output: [
 		{
