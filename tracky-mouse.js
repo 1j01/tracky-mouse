0e652c3b24cf252d372deda22fcc2e400b3a6bb3
Isaiah Odhner <isaiahodhner@gmail.com>
2022-08-05 17:39:14 -0400

    Add missing function from jspaint

--- tracky-mouse.js
+++ tracky-mouse.js
@@ -184,6 +184,17 @@ const init_dwell_clicking = (config) => {
 		};
 	};
 
+	const average_points = (points) => {
+		const average = { x: 0, y: 0 };
+		for (const pointer of points) {
+			average.x += pointer.x;
+			average.y += pointer.y;
+		}
+		average.x /= points.length;
+		average.y /= points.length;
+		return average;
+	};
+
 	const update = () => {
 		const time = Date.now();
 		recent_points = recent_points.filter((point_record) => time < point_record.time + averaging_window_timespan);
