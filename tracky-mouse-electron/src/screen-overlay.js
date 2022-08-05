fa11e851687f43f5d7abefd5191318a74754bdfb
Isaiah Odhner <isaiahodhner@gmail.com>
2022-08-05 18:23:50 -0400

    wip

--- tracky-mouse-electron/src/screen-overlay.js
+++ tracky-mouse-electron/src/screen-overlay.js
@@ -10,13 +10,19 @@ bigButton.style.height = "100%";
 bigButton.style.backgroundColor = "transparent";
 bigButton.style.border = "none";
 bigButton.id = "button-that-takes-up-the-entire-screen";
+document.body.appendChild(bigButton);
 
 TrackyMouse.initDwellClicking({
 	noCenter: (el) => el.matches("#button-that-takes-up-the-entire-screen"),
 	click: ({ x, y }) => {
 		mouseClick(x, y);
 	},
+	retarget: [], // not optional?
+	targets: "#button-that-takes-up-the-entire-screen",
+	isEquivalentTarget: (el1, el2) => el1 === el2, // not optional??
 });
+
+window.dispatchEvent(new Event("focus"));
 
 electronAPI.onToggle((event, isEnabled) => {
 	actionSpan.innerText = isEnabled ? "disable" : "enable";
