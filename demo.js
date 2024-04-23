c7077517ce3bc5a3e979f59ae8919ff69572a6dc
Isaiah Odhner <isaiahodhner@gmail.com>
2024-04-23 02:16:30 -0400

    Updates

--- demo.js
+++ demo.js
@@ -31,10 +31,10 @@ const config = {
 	// 	.window:not(.maximized) .window-titlebar
 	// `,
 	// Filter for elements to drag. They must be included in the targets first.
-	shouldDrag: (target) => (
-		target.matches(".window-titlebar") ||
-		(target.matches(".drawing-canvas") && current_tool.supports_drag)
-	),
+	// shouldDrag: (target) => (
+	// 	target.matches(".window-titlebar") ||
+	// 	(target.matches(".drawing-canvas") && current_tool.supports_drag)
+	// ),
 	// Instead of clicking in the center of these elements, click at any point within the element.
 	// This is useful for drag offsets, like for a window titlebar,
 	// and position-based inputs like sliders or color pickers, or a drawing canvas.
@@ -162,29 +162,185 @@ TrackyMouse.onPointerMove = (x, y) => {
 	target.dispatchEvent(event);
 };
 
-// Archery target hit animation
+// Archery mini-game
+const archery_game = document.getElementById("archery-demo");
+const archery_scoreboard = document.getElementById("archery-scoreboard");
 const archery_targets = document.querySelectorAll(".archery-target");
-for (const archery_target of archery_targets) {
-	archery_target.addEventListener("pointerdown", () => {
-		// archery_target.style.animation = "archery-target-hit 0.5s ease-in-out";
-		// archery_target.addEventListener("animationend", () => {
-		// 	archery_target.style.animation = "";
-		// }, { once: true });
-		const frames = [];
-		let angle = 0;
-		let angularVelocity = 2 + Math.random() * 0.2;
-		for (let t = 0; t < 100; t++) {
-			angularVelocity *= 0.92;
-			angle += angularVelocity;
-			angularVelocity += (Math.sin(angle)) * 0.1;
-			frames.push({
-				transform: `translate(-50%, -50%) rotateX(${angle}rad)`,
-				opacity: Math.min(1, Math.max(0.2, 1 - t / 100 * 4.123456) - Math.cos(angle) * 0.1),
-			});
+let round;
+const best_times = {
+	with_head_tracker: Infinity,
+	with_dwell_clicker: Infinity,
+	with_mouse: Infinity,
+	with_keyboard: Infinity,
+	with_unknown_input: Infinity,
+};
+function initRound() {
+	round = {
+		used_manual_movement: false, // non-head-tracker mouse movement
+		used_manual_click: false, // non-dwell clicking
+		used_keyboard: false, // not much of a game, but may be an interesting comparison
+		used_unknown_input: false, // click event despite preventing via keydown/pointerdown
+		start_time: undefined, // set when the first target is hit
+	};
+}
+initRound();
+let last_pointerdown_time = -Infinity;
+archery_game.addEventListener("pointerdown", (event) => {
+	if (event.pointerId !== 1234567890) {
+		// TODO: maybe only set if target was hit; could use a callback (or return value but that would be a little confusing since handleTargetHit looks like an event handler)
+		round.used_manual_click = true;
+	}
+	// Don't call `event.preventDefault()` because click will be triggered regardless, but it will prevent text deselection, which is very irritating.
+	handleTargetHit(event);
+	last_pointerdown_time = performance.now();
+});
+archery_game.addEventListener("keydown", (event) => {
+	if (event.key === " " || event.key === "Enter") {
+		event.preventDefault();
+		round.used_keyboard = true;
+		handleTargetHit(event);
+	}
+});
+archery_game.addEventListener("click", (event) => {
+	if (performance.now() - last_pointerdown_time < 100) {
+		return;
+	}
+	round.used_unknown_input = true;
+	handleTargetHit(event);
+});
+archery_game.addEventListener("pointerenter", (event) => {
+	if (event.pointerId === 1234567890) {
+		return;
+	}
+	round.used_manual_movement = true;
+});
+
+function get_scoreboard_slot() {
+	// TODO: differentiate pen input from mouse input... for both dwell clicking and manual clicking?
+	if (round.used_keyboard) {
+		// keyboard is the most like cheating, so it goes first
+		return "with_keyboard";
+	} else if (round.used_manual_click) {
+		// next easiest is clicking with a mouse
+		return "with_mouse";
+	} else if (round.used_manual_movement) {
+		// next easiest is moving the mouse but using the dwell clicker
+		return "with_dwell_clicker";
+	} else if (round.used_unknown_input) {
+		// this last one is a wild card; it's hard to say whether this condition should be last or first
+		// I imagine some other accessibility feature might trigger this
+		// Can simulate with:
+		// for (const archeryTarget of document.querySelectorAll(".archery-target")) {
+		// 	archeryTarget.dispatchEvent(new PointerEvent("click", { bubbles: true }));
+		// }
+		// Of course, that's the most like cheating of all, so it feels like it should be first,
+		// but I suspect if this occurs, outside of a test, it won't be due to cheating.
+		// Of course, the ranking by "easiness" is only in case you mix input methods in a single round.
+		// It might not be the best way to detect a different (unknown) input method.
+		// That is to say, there's a different argument for giving this priority.
+		return "with_unknown_input";
+	} else {
+		return "with_head_tracker";
+	}
+}
+
+/**
+ * @param {PointerEvent | KeyboardEvent} event 
+ */
+function handleTargetHit(event) {
+	if (!event.target.matches(".archery-target")) {
+		return;
+	}
+	if (archery_game.classList.contains("round-over")) {
+		return;
+	}
+	const archery_target = event.target;
+	archery_target.classList.add("hit");
+	animateTargetHit(archery_target).then(() => {
+		archery_target.classList.remove("hit");
+	});
+	if (!round.start_time) {
+		initRound(); // reset input detection to ignore spurious hovering before the round starts
+		round.start_time = performance.now();
+		archery_scoreboard.hidden = true;
+	}
+	if (document.querySelectorAll(".archery-target:not(.hit)").length === 0) {
+		const time = (performance.now() - round.start_time) / 1000;
+		archery_scoreboard.hidden = false;
+		archery_scoreboard.textContent = `Time: ${time.toFixed(2)}s`;
+		const slot = get_scoreboard_slot();
+		const new_best = time < best_times[slot];
+		if (new_best) {
+			best_times[slot] = time;
 		}
-		archery_target.animate(frames, {
+		archery_scoreboard.append(document.createElement("br"));
+		const p = document.createElement("p");
+		p.textContent = `Best times:`;
+		archery_scoreboard.append(p);
+		const ul = document.createElement("ul");
+		archery_scoreboard.append(ul);
+		for (const [id, time] of Object.entries(best_times)) {
+			if (time === Infinity) {
+				continue;
+			}
+			const label = {
+				with_head_tracker: "With Head Tracking",
+				with_dwell_clicker: "With Dwell Clicking",
+				with_mouse: "With Mouse Clicking",
+				with_keyboard: "With Keyboard",
+				with_unknown_input: "With Unknown Input",
+			}[id];
+			const li = document.createElement("li");
+			li.textContent = `${label}: ${time.toFixed(2)}s`;
+			if (slot === id && new_best) {
+				li.classList.add("new-best-time");
+				const new_best = document.createElement("span");
+				new_best.textContent = " New Best!";
+				li.append(new_best);
+			}
+			ul.append(li);
+		}
+		archery_game.classList.add("round-over");
+		setTimeout(() => {
+			for (const archery_target of archery_targets) {
+				for (const animation of archery_target.getAnimations()) {
+					animation.cancel();
+				}
+			}
+			setTimeout(() => {
+				archery_game.classList.remove("round-over");
+				initRound();
+			}, 100);
+		}, 2000);
+	}
+}
+
+/**
+ * @param {HTMLButtonElement} archery_target 
+ */
+async function animateTargetHit(archery_target) {
+	// archery_target.style.animation = "archery-target-hit 0.5s ease-in-out";
+	// archery_target.addEventListener("animationend", () => {
+	// 	archery_target.style.animation = "";
+	// }, { once: true });
+	const frames = [];
+	let angle = 0;
+	let angularVelocity = 2 + Math.random() * 0.2;
+	for (let t = 0; t < 100; t++) {
+		angularVelocity *= 0.92;
+		angle += angularVelocity;
+		angularVelocity += (Math.sin(angle)) * 0.1;
+		frames.push({
+			transform: `translate(-50%, -50%) rotateX(${angle}rad)`,
+			opacity: Math.min(1, Math.max(0.2, 1 - t / 100 * 4.123456) - Math.cos(angle) * 0.1),
+		});
+	}
+	try {
+		await archery_target.animate(frames, {
 			duration: 10000,
 			easing: "linear",
-		});
-	});
+		}).finished;
+	} catch (error) {
+		// ignore cancelation
+	}
 }
