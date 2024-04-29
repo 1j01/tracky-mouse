89fcbd36946adc183c7e73fd88736a8a727edcd3
Isaiah Odhner <isaiahodhner@gmail.com>
2024-04-29 13:56:07 -0400

    Updates

--- demo.js
+++ demo.js
@@ -170,14 +170,22 @@ let round;
 const best_times = {
 	with_head_tracker: Infinity,
 	with_dwell_clicker: Infinity,
+	with_dwell_clicker_touch: Infinity, // unlikely, since touch doesn't have hovering (except on a few phones, as a gimmick; dunno if they trigger events with pointerType "touch" for hovering)
+	with_dwell_clicker_pen: Infinity,
 	with_mouse: Infinity,
+	with_touch: Infinity,
+	with_pen: Infinity,
 	with_keyboard: Infinity,
 	with_unknown_input: Infinity,
 };
 function initRound() {
 	round = {
 		used_manual_movement: false, // non-head-tracker mouse movement
+		used_manual_movement_touch: false, // non-head-tracker mouse movement
+		used_manual_movement_pen: false, // non-head-tracker mouse movement
 		used_manual_click: false, // non-dwell clicking
+		used_manual_click_touch: false, // non-dwell clicking
+		used_manual_click_pen: false, // non-dwell clicking
 		used_keyboard: false, // not much of a game, but may be an interesting comparison
 		used_unknown_input: false, // click event despite preventing via keydown/pointerdown
 		start_time: undefined, // set when the first target is hit
@@ -192,6 +200,11 @@ archery_game.addEventListener("pointerdown", (event) => {
 	if (event.pointerId !== 1234567890) {
 		// TODO: maybe only set if target was hit; could use a callback (or return value but that would be a little confusing since handleTargetHit looks like an event handler)
 		round.used_manual_click = true;
+		if (event.pointerType === "pen") {
+			round.used_manual_click_pen = true;
+		} else if (event.pointerType === "touch") {
+			round.used_manual_click_touch = true;
+		}
 	}
 	// Don't call `event.preventDefault()` because click will be triggered regardless, but it will prevent text deselection, which is very irritating.
 	handleTargetHit(event);
@@ -216,18 +229,43 @@ archery_game.addEventListener("pointerenter", (event) => {
 		return;
 	}
 	round.used_manual_movement = true;
+	if (event.pointerType === "pen") {
+		round.used_manual_movement_pen = true;
+	} else if (event.pointerType === "touch") {
+		round.used_manual_movement_touch = true;
+	}
 });
 
+/**
+ * @returns {keyof typeof best_times} scoreboard_slot - the slot in the scoreboard for the current input method (the most powerful, if it's ambiguous)
+ * 
+ * If you for example use the head tracker for one target and then mash Tab+Enter for the rest, that should count as using the keyboard.
+ * 
+ * Note that pointerType may be "mouse" when using a pen, under some circumstances.
+ * It seems I need to enable "Windows Ink" in the Wacom settings (and re-open the tab, not just reload) to get "pen" as the pointerType.
+ */
 function get_scoreboard_slot() {
-	// TODO: differentiate pen input from mouse input... for both dwell clicking and manual clicking?
 	if (round.used_keyboard) {
 		// keyboard is the most like cheating, so it goes first
 		return "with_keyboard";
+	} else if (round.used_manual_click_touch) {
+		// next easiest is clicking with touch (it's an absolute input method, and you can see exactly where you're clicking; you could even line up multiple fingers with targets... actually that might even make it more powerful than the keyboard...)
+		return "with_touch";
+	} else if (round.used_manual_click_pen) {
+		// next easiest is clicking with a pen (it's an absolute input method, but you have to look at the cursor to see where you're clicking)
+		return "with_pen";
 	} else if (round.used_manual_click) {
-		// next easiest is clicking with a mouse
+		// next easiest is clicking with a mouse (it's relative, but precise and familiar)
 		return "with_mouse";
+	} else if (round.used_manual_movement_touch) {
+		// next easiest is moving the mouse but using the dwell clicker (it's precise but slower)
+		// not sure about touch vs pen/mouse in this case, as I don't have a touch screen supporting hover
+		return "with_dwell_clicker_touch";
+	} else if (round.used_manual_movement_pen) {
+		// (see previous comment)
+		return "with_dwell_clicker_pen";
 	} else if (round.used_manual_movement) {
-		// next easiest is moving the mouse but using the dwell clicker
+		// (see previous comment)
 		return "with_dwell_clicker";
 	} else if (round.used_unknown_input) {
 		// this last one is a wild card; it's hard to say whether this condition should be last or first
@@ -236,8 +274,8 @@ function get_scoreboard_slot() {
 		// for (const archeryTarget of document.querySelectorAll(".archery-target")) {
 		// 	archeryTarget.dispatchEvent(new PointerEvent("click", { bubbles: true }));
 		// }
-		// Of course, that's the most like cheating of all, so it feels like it should be first,
-		// but I suspect if this occurs, outside of a test, it won't be due to cheating.
+		// Of course, that example is completely cheating, which makes it feel like it should be first,
+		// but I suspect if this occurs outside of a test, it won't be due to cheating.
 		// Of course, the ranking by "easiness" is only in case you mix input methods in a single round.
 		// It might not be the best way to detect a different (unknown) input method.
 		// That is to say, there's a different argument for giving this priority.
@@ -246,6 +284,18 @@ function get_scoreboard_slot() {
 		return "with_head_tracker";
 	}
 }
+
+const slot_labels = {
+	with_head_tracker: "With Head Tracking",
+	with_dwell_clicker: "With Dwell Clicking", // may be pen, undetectable in some cases
+	with_dwell_clicker_touch: "With Dwell Clicking (Touch)",
+	with_dwell_clicker_pen: "With Dwell Clicking (Pen)",
+	with_mouse: "With Manual Clicking", // may be pen, undetectable in some cases, hence "manual" instead of "mouse"
+	with_touch: "With Touch",
+	with_pen: "With Pen",
+	with_keyboard: "With Keyboard",
+	with_unknown_input: "With Unknown Input",
+};
 
 /**
  * @param {PointerEvent | KeyboardEvent} event 
@@ -277,23 +327,21 @@ function handleTargetHit(event) {
 		if (new_best) {
 			best_times[slot] = time;
 		}
-		archery_scoreboard.append(document.createElement("br"));
-		const p = document.createElement("p");
-		p.textContent = `Best times:`;
-		archery_scoreboard.append(p);
+		const slot_indicator = document.createElement("p");
+		slot_indicator.textContent = slot_labels[slot];
+		slot_indicator.classList.add("slot-indicator");
+		archery_scoreboard.append(slot_indicator);
+		const best_times_label = document.createElement("p");
+		best_times_label.textContent = `Best times:`;
+		best_times_label.classList.add("best-times-label");
+		archery_scoreboard.append(best_times_label);
 		const ul = document.createElement("ul");
 		archery_scoreboard.append(ul);
 		for (const [id, time] of Object.entries(best_times)) {
 			if (time === Infinity) {
 				continue;
 			}
-			const label = {
-				with_head_tracker: "With Head Tracking",
-				with_dwell_clicker: "With Dwell Clicking",
-				with_mouse: "With Mouse Clicking",
-				with_keyboard: "With Keyboard",
-				with_unknown_input: "With Unknown Input",
-			}[id];
+			const label = slot_labels[id];
 			const li = document.createElement("li");
 			li.textContent = `${label}: ${time.toFixed(2)}s`;
 			if (slot === id && new_best) {
