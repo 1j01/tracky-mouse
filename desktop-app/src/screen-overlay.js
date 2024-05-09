1j01/tracky-mouse

commit 5659495bb7b52af752854627ba2957b5ae3cd0e3

Show indicator when no face is found
Partially addresses https://github.com/1j01/tracky-mouse/issues/26
Also working towards https://github.com/1j01/tracky-mouse/issues/41
and https://github.com/1j01/tracky-mouse/issues/49

desktop-app/src/screen-overlay.js
@@ -39,13 +39,21 @@ electronAPI.onMouseMove((_event, x, y) => {
 	window.dispatchEvent(domEvent);
 });
 
-electronAPI.onChangeDwellClicking((_event, isEnabled, isManualTakeback) => {
+electronAPI.onChangeDwellClicking((_event, isEnabled, isManualTakeback, cameraFeedDiagnostics) => {
 	// console.log("onChangeDwellClicking", isEnabled);
+
+	// Other diagnostics in the future would be stuff like:
+	// - head too far away (smaller than a certain size) https://github.com/1j01/tracky-mouse/issues/49
+	// - bad lighting conditions
+	// see: https://github.com/1j01/tracky-mouse/issues/26
+
 	document.body.classList.toggle("tracky-mouse-manual-takeback", isManualTakeback);
+	document.body.classList.toggle("tracky-mouse-head-not-found", cameraFeedDiagnostics.headNotFound);
 	actionSpan.innerText = isEnabled ? "disable" : "enable";
 
 	if (!isEnabled && !isManualTakeback) {
 		// Fade out the message after a little while so it doesn't get in the way.
+		// TODO: make sure animation isn't interrupted by cameraFeedDiagnostics updates.
 		message.style.animation = "tracky-mouse-screen-overlay-message-fade-out 2s ease-in-out forwards 10s";
 	} else {
 		message.style.animation = "";

core/tracky-mouse.css
@@ -246,6 +246,12 @@
 	transform: translate(-50%, -50%);
 }
 
+/* prefer manual takeback indicator, rather than showing both at once */
+body.tracky-mouse-manual-takeback .tracky-mouse-head-not-found-indicator,
+body:not(.tracky-mouse-head-not-found) .tracky-mouse-head-not-found-indicator {
+	display: none;
+}
+
 body:not(.tracky-mouse-manual-takeback) .tracky-mouse-manual-takeback-indicator {
 	display: none;
 }
@@ -254,7 +260,7 @@ body:not(.tracky-mouse-manual-takeback) .tracky-mouse-manual-takeback-indicator 
 	display: none;
 }
 
-.tracky-mouse-manual-takeback-indicator img {
+.tracky-mouse-screen-overlay-status-indicator img {
 	filter: drop-shadow(0 0 4px #000) drop-shadow(2px 2px 2px #000);
 	opacity: 0.5;
 }

core/tracky-mouse.js
@@ -1573,6 +1573,10 @@ TrackyMouse.init = function (div) {
 			mainOops.update(imageData);
 		}
 
+		if (window.electronAPI) {
+			window.electronAPI.notifyCameraFeedDiagnostics({ headNotFound: !face && !facemeshPrediction });
+		}
+
 		if (facemeshPrediction) {
 			ctx.fillStyle = "red";
 

desktop-app/images/head-not-found.svg
@@ -1,0 +1,135 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   width="64.0px"
+   height="64.0px"
+   viewBox="0 0 64.0 64.0"
+   version="1.1"
+   id="SVGRoot"
+   sodipodi:docname="head-not-found.svg"
+   inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <sodipodi:namedview
+     id="namedview1054"
+     pagecolor="#505050"
+     bordercolor="#ffffff"
+     borderopacity="1"
+     inkscape:showpageshadow="0"
+     inkscape:pageopacity="0"
+     inkscape:pagecheckerboard="1"
+     inkscape:deskcolor="#505050"
+     inkscape:document-units="px"
+     showgrid="true"
+     inkscape:zoom="5.828192"
+     inkscape:cx="18.787988"
+     inkscape:cy="18.616408"
+     inkscape:current-layer="layer1"
+     inkscape:window-width="1920"
+     inkscape:window-height="1009"
+     inkscape:window-x="-8"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1">
+    <inkscape:grid
+       type="xygrid"
+       id="grid1540"
+       empspacing="8"
+       originx="0"
+       originy="0"
+       spacingy="1"
+       spacingx="1"
+       units="px"
+       visible="true" />
+  </sodipodi:namedview>
+  <defs
+     id="defs1049" />
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <g
+       id="g3699"
+       transform="translate(0.44861459,3.114008)"
+       inkscape:label="head/face">
+      <path
+         style="fill:none;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000;stroke-dasharray:1,3;stroke-dashoffset:0"
+         d="m 30.141868,12.978181 c 14.494394,-1.150235 14.279704,11.10915 12.398736,17.726188 -1.811897,6.374057 -1.014423,8.596418 -3.989219,11.681623 C 35.57659,45.471196 28.068583,46.316195 24.159832,42.123198 21.531192,39.303401 21.197535,35.865302 19.751164,31.11657 17.067937,22.306985 19.888772,13.791838 30.141868,12.978181 Z"
+         id="path1660"
+         sodipodi:nodetypes="ssssss"
+         inkscape:label="head outline" />
+      <path
+         style="fill:none;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000"
+         d="m 34.049842,27.080668 c 0.01551,1.045547 0.30038,1.817774 0.538426,2.018566 2.96671,2.50248 -1.324581,6.211798 -3.858385,3.634784"
+         id="path3273"
+         sodipodi:nodetypes="csc"
+         inkscape:label="nose" />
+      <path
+         style="fill:none;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000;stroke-dasharray:1,3;stroke-dashoffset:0"
+         d="m 37.164979,36.569026 c -1.967047,3.259348 -6.756567,4.047549 -9.731686,0.636254"
+         id="path3273-3"
+         sodipodi:nodetypes="cc"
+         inkscape:label="mouth" />
+      <path
+         d="m 29.303443,26.583633 c -0.286386,-3.512331 -5.95152,-3.14935 -6.125787,0.0025 1.177451,1.011134 4.143639,1.403238 6.125787,-0.0025 z"
+         style="fill:none;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000"
+         id="path3547"
+         sodipodi:nodetypes="ccc"
+         inkscape:label="eye" />
+      <path
+         d="m 40.199316,26.313676 c -0.388768,-3.372612 -6.017232,-3.133673 -6.152333,0.105298 1.343476,1.302451 4.337462,1.588922 6.152333,-0.105298 z"
+         style="fill:none;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000"
+         id="path3547-3"
+         sodipodi:nodetypes="ccc"
+         inkscape:label="eye" />
+      <circle
+         style="fill:#ffffff;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000"
+         id="path3815"
+         cx="26.390192"
+         cy="26.663704"
+         r="0.75630671"
+         inkscape:label="pupil" />
+      <circle
+         style="fill:#ffffff;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000"
+         id="path3815-2"
+         cx="37.200237"
+         cy="26.602921"
+         r="0.75630671"
+         inkscape:label="pupil" />
+      <path
+         style="fill:none;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000"
+         d="m 23.975227,24.831052 c 2.776779,-1.22921 3.865192,-0.537986 5.217286,-0.146685"
+         id="path3942"
+         sodipodi:nodetypes="cc"
+         inkscape:label="eye brow"
+         transform="translate(-0.44861459,-3.114008)" />
+      <path
+         style="fill:none;stroke:#ffffff;stroke-width:1;stroke-linecap:square;stop-color:#000000"
+         d="m 34.747591,24.816768 c 2.776779,-1.22921 3.865192,-0.537986 5.217286,-0.146685"
+         id="path3942-6"
+         sodipodi:nodetypes="cc"
+         inkscape:label="eye brow"
+         transform="translate(-0.44861459,-3.114008)" />
+    </g>
+    <g
+       id="g7"
+       inkscape:label="magnifying glass"
+       transform="rotate(-35.29423,47.928286,36.354572)">
+      <circle
+         style="fill:none;stroke:#f9f9f9;stroke-linecap:round;-inkscape-stroke:none"
+         id="path4"
+         cx="43.5"
+         cy="36.5"
+         r="9"
+         inkscape:label="lens" />
+      <path
+         style="fill:none;stroke:#f9f9f9;stroke-linecap:round;-inkscape-stroke:none"
+         d="m 43,46 v 10 c 0,0 0,1 1,1 1,0 1,-1 1,-1 V 46"
+         id="path7"
+         sodipodi:nodetypes="ccscc"
+         inkscape:label="handle" />
+    </g>
+  </g>
+</svg>

desktop-app/src/electron-main.js
@@ -237,11 +237,13 @@ const createWindow = () => {
 	const regainControlForTime = 2000; // in milliseconds, AFTER the mouse hasn't moved for more than mouseMoveRequestHistoryDuration milliseconds (I think)
 	let regainControlTimeout = null; // also used to check if we're pausing temporarily
 	let enabled = true; // for starting/stopping until the user requests otherwise
+	let cameraFeedDiagnostics = {};
 	const updateDwellClicking = () => {
 		screenOverlayWindow.webContents.send(
 			'change-dwell-clicking',
 			enabled && regainControlTimeout === null,
-			enabled && regainControlTimeout !== null
+			enabled && regainControlTimeout !== null,
+			cameraFeedDiagnostics,
 		);
 	};
 	ipcMain.on('move-mouse', async (_event, x, y, time) => {
@@ -306,6 +308,11 @@ const createWindow = () => {
 			mousePosHistory.push({ point: { x: initialPos.x, y: initialPos.y }, time: performance.now(), from: "notify-toggle-state" });
 		}
 	});
+	ipcMain.on('notify-camera-feed-diagnostics', (_event, data) => {
+		cameraFeedDiagnostics = data;
+		updateDwellClicking();
+	});
+
 
 	ipcMain.on('set-options', (_event, newOptions) => {
 		deserializeSettings(newOptions);

desktop-app/src/electron-screen-overlay.html
@@ -12,8 +12,13 @@
 </head>
 
 <body>
-	<div class="tracky-mouse-absolute-center tracky-mouse-manual-takeback-indicator">
-		<img src="../images/manual-takeback.svg" alt="hand reaching for mouse" width="128" height="128">
+	<div class="tracky-mouse-absolute-center">
+		<div class="tracky-mouse-screen-overlay-status-indicator tracky-mouse-manual-takeback-indicator">
+			<img src="../images/manual-takeback.svg" alt="hand reaching for mouse" width="128" height="128">
+		</div>
+		<div class="tracky-mouse-screen-overlay-status-indicator tracky-mouse-head-not-found-indicator">
+			<img src="../images/head-not-found.svg" alt="head not found" width="128" height="128">
+		</div>
 	</div>
 	<div id="tracky-mouse-screen-overlay-message">
 		<div class="tracky-mouse-manual-takeback-indicator">

desktop-app/src/preload-app-window.js
@@ -21,6 +21,10 @@ contextBridge.exposeInMainWorld("electronAPI", {
 		ipcRenderer.send('notify-toggle-state', nowEnabled);
 	},
 
+	notifyCameraFeedDiagnostics: (data) => {
+		ipcRenderer.send('notify-camera-feed-diagnostics', data);
+	},
+
 	setOptions: (optionsPatch) => {
 		ipcRenderer.send('set-options', optionsPatch);
 	},

commit a018e3586e0b7f5a5f66cdc771eedf68c8b1bbf5

Fix dwell clicking in desktop app
Broken in: 5659495bb7b52af752854627ba2957b5ae3cd0e3

Sending these events blocks the dwell clicker from working.
Since the onChangeDwellClicking callback is now called frequently,
we need to check that it's actually pausing/unpausing first.

desktop-app/src/screen-overlay.js
@@ -39,8 +39,9 @@ electronAPI.onMouseMove((_event, x, y) => {
 	window.dispatchEvent(domEvent);
 });
 
+let wasEnabled = false;
 electronAPI.onChangeDwellClicking((_event, isEnabled, isManualTakeback, cameraFeedDiagnostics) => {
-	// console.log("onChangeDwellClicking", isEnabled);
+	console.log("onChangeDwellClicking", isEnabled, isManualTakeback, cameraFeedDiagnostics);
 
 	// Other diagnostics in the future would be stuff like:
 	// - head too far away (smaller than a certain size) https://github.com/1j01/tracky-mouse/issues/49
@@ -61,6 +62,10 @@ electronAPI.onChangeDwellClicking((_event, isEnabled, isManualTakeback, cameraFe
 	}
 
 	// "Trick" Tracky Mouse into stopping/starting the dwell clicker.
-	document.dispatchEvent(new Event(isEnabled ? "mouseenter" : "mouseleave"));
-	window.dispatchEvent(new Event(isEnabled ? "focus" : "blur"));
+	// (TODO: can I use the return value of initDwellClicking instead? Or otherwise formalize this?)
+	if (wasEnabled !== isEnabled) {
+		document.dispatchEvent(new Event(isEnabled ? "mouseenter" : "mouseleave"));
+		window.dispatchEvent(new Event(isEnabled ? "focus" : "blur"));
+	}
+	wasEnabled = isEnabled;
 });