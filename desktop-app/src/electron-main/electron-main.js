447ddd4ebf94a69b379c0bbc2cebf3b090eb9709
Isaiah Odhner <isaiahodhner@gmail.com>
2024-10-14 02:38:01 -0400

    Improve error handling for --version

--- desktop-app/src/electron-main/electron-main.js
+++ desktop-app/src/electron-main/electron-main.js
@@ -116,15 +116,16 @@ if (args.version) {
 	let version = require("../../package.json").version;
 	if (isGitRepo) {
 		const { execSync } = require('child_process');
+		const describeCommand = "git describe --tags";
 		try {
-			const describeOutput = execSync('git describe --tags', { cwd: __dirname }).toString().trim();
+			// By default, execSync inherits stderr, but we want to capture it for a nicer error message.
+			// (It says the default is "pipe", which is equivalent to ["pipe", "pipe", "pipe"].
+			// But it seems to be ["pipe", "pipe", "inherit"] in practice.)
+			const describeOutput = execSync(describeCommand, { cwd: __dirname, stdio: ['pipe', 'pipe', 'pipe'] }).toString().trim();
 			version = "development " + describeOutput;
-		} catch (_error) {
-			// Stderr is already printed to the console by default (configurable with `stdio` option).
-			// The error object might give a little more info, like the exit code, but it also gives a lot of noise.
-			// Could refine this by silencing stderr and printing a nicer error message containing the stderr output, the command, and maybe the exit code.
-			// (error.stderr.toString() and error.status can be used for that)
-			// console.error("Failed to get `git describe --tags` output:", error);
+		} catch (error) {
+			const stderrOutput = error.stderr ? error.stderr.toString().trim() : '(No stderr output)';
+			console.error(`Failed to get \`${describeCommand}\` output (Exit code: ${error.status ?? "unknown"}):\n${stderrOutput}`);
 		}
 	}
 
