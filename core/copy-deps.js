f89cc328b4adcacbd25d5e50db6bfd3070979d10
Isaiah Odhner <isaiahodhner@gmail.com>
2024-12-21 15:55:31 -0500

    some map files

--- core/copy-deps.js
+++ core/copy-deps.js
@@ -10,6 +10,8 @@ fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_packed_ass
 fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.wasm", "lib/face_mesh/face_mesh_solution_simd_wasm_bin.wasm");
 fs.copyFileSync("node_modules/@tensorflow-models/face-landmarks-detection/dist/face-landmarks-detection.min.js", "lib/face-landmarks-detection.min.js");
 fs.copyFileSync("node_modules/@tensorflow/tfjs-core/dist/tf-core.min.js", "lib/tf-core.min.js");
+fs.copyFileSync("node_modules/@tensorflow/tfjs-core/dist/tf-core.min.js.map", "lib/tf-core.min.js.map");
 fs.copyFileSync("node_modules/@tensorflow/tfjs-backend-webgl/dist/tf-backend-webgl.min.js", "lib/tf-backend-webgl.min.js");
+fs.copyFileSync("node_modules/@tensorflow/tfjs-backend-webgl/dist/tf-backend-webgl.min.js.map", "lib/tf-backend-webgl.min.js.map");
 
 console.log("Dependencies copied successfully!");
