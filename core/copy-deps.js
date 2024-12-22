bef23c5c9cb110439fac03217d581e188486e91d
Isaiah Odhner <isaiahodhner@gmail.com>
2024-12-21 19:50:37 -0500

    copy non-SIMD deps

--- core/copy-deps.js
+++ core/copy-deps.js
@@ -11,9 +11,11 @@ fs.copyFileSync("node_modules/@tensorflow-models/face-landmarks-detection/dist/f
 
 fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh.js", "lib/face_mesh/face_mesh.js");
 fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_packed_assets_loader.js", "lib/face_mesh/face_mesh_solution_packed_assets_loader.js");
-fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.js", "lib/face_mesh/face_mesh_solution_simd_wasm_bin.js");
 fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh.binarypb", "lib/face_mesh/face_mesh.binarypb");
 fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_packed_assets.data", "lib/face_mesh/face_mesh_solution_packed_assets.data");
+fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.js", "lib/face_mesh/face_mesh_solution_simd_wasm_bin.js");
 fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.wasm", "lib/face_mesh/face_mesh_solution_simd_wasm_bin.wasm");
+fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_wasm_bin.js", "lib/face_mesh/face_mesh_solution_wasm_bin.js");
+fs.copyFileSync("node_modules/@mediapipe/face_mesh/face_mesh_solution_wasm_bin.wasm", "lib/face_mesh/face_mesh_solution_wasm_bin.wasm");
 
 console.log("Dependencies copied successfully!");
