6b3a1c79df72a632dbd712f721cdb9f2e21e2d51
Isaiah Odhner <isaiahodhner@gmail.com>
2026-01-18 08:40:25 -0500

    Reorganize variables

--- core/tracky-mouse.js
+++ core/tracky-mouse.js
@@ -783,35 +783,13 @@ TrackyMouse.init = function (div, { statsJs = false } = {}) {
 		document.body.appendChild(stats.domElement);
 	}
 
-	var defaultWidth = 640;
-	var defaultHeight = 480;
-	var maxPoints = 1000;
-	var mouseX = 0;
-	var mouseY = 0;
-	var cameraFramesSinceFacemeshUpdate = [];
+	// Settings (initialized from HTML template defaults, then overridden by any saved settings)
 	var sensitivityX;
 	var sensitivityY;
 	var acceleration;
 	var minDistance;
 	var tiltInfluence;
 	var delayBeforeDragging;
-	var buttonStates = {
-		left: false,
-		right: false,
-	};
-	var lastMouseDownTime = -Infinity;
-	var face;
-	var faceScore = 0;
-	var faceScoreThreshold = 0.5;
-	var faceConvergence = 0;
-	// var faceConvergenceThreshold = 50;
-	var pointsBasedOnFaceScore = 0;
-	var paused = true;
-	var mouseNeedsInitPos = true;
-	var debugAcceleration = false;
-	var showDebugText = false;
-	var showDebugEyelidContours = false;
-	var showDebugEyeZoom = true;
 	var mirror;
 	var cameraDeviceId;
 	var startEnabled;
@@ -819,9 +797,17 @@ TrackyMouse.init = function (div, { statsJs = false } = {}) {
 	var swapMouseButtons;
 	var clickingMode;
 
-	var useClmTracking = true;
-	var showClmTracking = useClmTracking;
-	var useFacemesh = true;
+	// Debug flags (not shown in the UI; could become Advanced Settings in the future)
+	var debugAcceleration = false;
+	var showDebugText = false;
+	var showDebugEyelidContours = false;
+	var showDebugEyeZoom = true;
+
+	// Constants (could become Advanced Settings in the future)
+	var defaultWidth = 640;
+	var defaultHeight = 480;
+	var maxPoints = 1000;
+	var faceScoreThreshold = 0.5;
 	var facemeshOptions = {
 		maxContinuousChecks: 5,
 		detectionConfidence: 0.9,
@@ -829,8 +815,21 @@ TrackyMouse.init = function (div, { statsJs = false } = {}) {
 		iouThreshold: 0.3,
 		scoreThreshold: 0.75
 	};
-	var fallbackTimeoutID;
+	var useFacemesh = true;
+	// maybe should be based on size of head in view?
+	const pruningGridSize = 5;
+	const minDistanceToAddPoint = pruningGridSize * 1.5;
 
+	// Head tracking and facial gesture state
+	// ## Clmtrackr state
+	var face;
+	var faceScore = 0;
+	var faceConvergence = 0;
+	// var faceConvergenceThreshold = 50;
+	var pointsBasedOnFaceScore = 0;
+	// ## Facemesh state
+	let detector;
+	let currentCameraImageData;
 	var facemeshLoaded = false;
 	var facemeshFirstEstimation = true;
 	var facemeshEstimating = false;
@@ -839,14 +838,80 @@ TrackyMouse.init = function (div, { statsJs = false } = {}) {
 	var facemeshEstimateFaces;
 	var faceInViewConfidenceThreshold = 0.7;
 	var pointsBasedOnFaceInViewConfidence = 0;
+	var cameraFramesSinceFacemeshUpdate = [];
 	var blinkInfo;
 	var mouthInfo;
 	var headTilt = { pitch: 0, yaw: 0, roll: 0 };
+	// ## State related to switching between head trackers
+	var useClmTracking = true;
+	var showClmTracking = useClmTracking;
+	var fallbackTimeoutID;
 
+	// Mouse state
+	var mouseX = 0;
+	var mouseY = 0;
+	var buttonStates = {
+		left: false,
+		right: false,
+	};
+	var lastMouseDownTime = -Infinity;
+	var mouseNeedsInitPos = true;
+
+	// Other state
+	var paused = true;
 	var pointTracker;
 
-	let currentCameraImageData;
-	let detector;
+	// Named lists of facemesh landmark indices
+	const MESH_ANNOTATIONS = {
+		silhouette: [
+			10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
+			397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
+			172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109
+		],
+
+		lipsUpperOuter: [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291],
+		lipsLowerOuter: [146, 91, 181, 84, 17, 314, 405, 321, 375, 291],
+		lipsUpperInner: [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308],
+		lipsLowerInner: [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308],
+
+		rightEyeUpper0: [246, 161, 160, 159, 158, 157, 173],
+		rightEyeLower0: [33, 7, 163, 144, 145, 153, 154, 155, 133],
+		rightEyeUpper1: [247, 30, 29, 27, 28, 56, 190],
+		rightEyeLower1: [130, 25, 110, 24, 23, 22, 26, 112, 243],
+		rightEyeUpper2: [113, 225, 224, 223, 222, 221, 189],
+		rightEyeLower2: [226, 31, 228, 229, 230, 231, 232, 233, 244],
+		rightEyeLower3: [143, 111, 117, 118, 119, 120, 121, 128, 245],
+
+		rightEyebrowUpper: [156, 70, 63, 105, 66, 107, 55, 193],
+		rightEyebrowLower: [35, 124, 46, 53, 52, 65],
+
+		rightEyeIris: [473, 474, 475, 476, 477],
+
+		leftEyeUpper0: [466, 388, 387, 386, 385, 384, 398],
+		leftEyeLower0: [263, 249, 390, 373, 374, 380, 381, 382, 362],
+		leftEyeUpper1: [467, 260, 259, 257, 258, 286, 414],
+		leftEyeLower1: [359, 255, 339, 254, 253, 252, 256, 341, 463],
+		leftEyeUpper2: [342, 445, 444, 443, 442, 441, 413],
+		leftEyeLower2: [446, 261, 448, 449, 450, 451, 452, 453, 464],
+		leftEyeLower3: [372, 340, 346, 347, 348, 349, 350, 357, 465],
+
+		leftEyebrowUpper: [383, 300, 293, 334, 296, 336, 285, 417],
+		leftEyebrowLower: [265, 353, 276, 283, 282, 295],
+
+		leftEyeIris: [468, 469, 470, 471, 472],
+
+		midwayBetweenEyes: [168],
+
+		noseTip: [1],
+		noseBottom: [2],
+		noseRightCorner: [98],
+		noseLeftCorner: [327],
+
+		rightCheek: [205],
+		leftCheek: [425]
+	};
+
+
 	const initFacemesh = async () => {
 		if (detector) {
 			detector.dispose();
@@ -1390,10 +1455,6 @@ TrackyMouse.init = function (div, { statsJs = false } = {}) {
 	// 	}
 	// }
 
-	// maybe should be based on size of head in view?
-	const pruningGridSize = 5;
-	const minDistanceToAddPoint = pruningGridSize * 1.5;
-
 	// Object Oriented Programming Sucks
 	// or Optical flOw Points System
 	class OOPS {
@@ -1713,55 +1774,6 @@ TrackyMouse.init = function (div, { statsJs = false } = {}) {
 							facemeshPrediction.keypoints[index] ?
 								[facemeshPrediction.keypoints[index].x, facemeshPrediction.keypoints[index].y, facemeshPrediction.keypoints[index].z] :
 								undefined;
-
-						const MESH_ANNOTATIONS = {
-							silhouette: [
-								10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
-								397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
-								172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109
-							],
-
-							lipsUpperOuter: [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291],
-							lipsLowerOuter: [146, 91, 181, 84, 17, 314, 405, 321, 375, 291],
-							lipsUpperInner: [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308],
-							lipsLowerInner: [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308],
-
-							rightEyeUpper0: [246, 161, 160, 159, 158, 157, 173],
-							rightEyeLower0: [33, 7, 163, 144, 145, 153, 154, 155, 133],
-							rightEyeUpper1: [247, 30, 29, 27, 28, 56, 190],
-							rightEyeLower1: [130, 25, 110, 24, 23, 22, 26, 112, 243],
-							rightEyeUpper2: [113, 225, 224, 223, 222, 221, 189],
-							rightEyeLower2: [226, 31, 228, 229, 230, 231, 232, 233, 244],
-							rightEyeLower3: [143, 111, 117, 118, 119, 120, 121, 128, 245],
-
-							rightEyebrowUpper: [156, 70, 63, 105, 66, 107, 55, 193],
-							rightEyebrowLower: [35, 124, 46, 53, 52, 65],
-
-							rightEyeIris: [473, 474, 475, 476, 477],
-
-							leftEyeUpper0: [466, 388, 387, 386, 385, 384, 398],
-							leftEyeLower0: [263, 249, 390, 373, 374, 380, 381, 382, 362],
-							leftEyeUpper1: [467, 260, 259, 257, 258, 286, 414],
-							leftEyeLower1: [359, 255, 339, 254, 253, 252, 256, 341, 463],
-							leftEyeUpper2: [342, 445, 444, 443, 442, 441, 413],
-							leftEyeLower2: [446, 261, 448, 449, 450, 451, 452, 453, 464],
-							leftEyeLower3: [372, 340, 346, 347, 348, 349, 350, 357, 465],
-
-							leftEyebrowUpper: [383, 300, 293, 334, 296, 336, 285, 417],
-							leftEyebrowLower: [265, 353, 276, 283, 282, 295],
-
-							leftEyeIris: [468, 469, 470, 471, 472],
-
-							midwayBetweenEyes: [168],
-
-							noseTip: [1],
-							noseBottom: [2],
-							noseRightCorner: [98],
-							noseLeftCorner: [327],
-
-							rightCheek: [205],
-							leftCheek: [425]
-						};
 
 						const annotations = Object.fromEntries(Object.entries(MESH_ANNOTATIONS).map(([key, indices]) => {
 							return [key, indices.map(getPoint)];
