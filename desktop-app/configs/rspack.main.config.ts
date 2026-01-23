8eca1c6850d7a6420bece76a161e67bc30bdc860
Isaiah Odhner <isaiahodhner@gmail.com>
2026-01-23 08:44:34 -0500

    Copy configs/ from electron-forge-rspack-template

---
+++ desktop-app/configs/rspack.main.config.ts
@@ -0,0 +1,23 @@
+import type { Configuration } from '@rspack/core';
+
+import { isProduction } from './rspack.env';
+import { plugins } from './rspack.plugins';
+import { rules } from './rspack.rules';
+import { pathResolve } from './utils';
+
+export const mainConfig: Configuration = {
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  entry: './src/main.ts',
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+  plugins,
+  resolve: {
+    tsConfig: pathResolve('tsconfig.json'),
+  },
+  devtool: isProduction ? false : 'eval-cheap-module-source-map',
+};
