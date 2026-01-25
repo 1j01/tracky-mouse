1127fb84499df2c5215c0013c2cb2650852710b0
Isaiah Odhner <isaiahodhner@gmail.com>
2026-01-24 21:30:30 -0500

    Work around symlink bug in Electron Forge
    Copy the `tracky-mouse` (core) module into the `node_modules` folder
    of the packaged app in a `packageAfterPrune` hook in the build process.
    
    This works around a bug in Electron Forge where dependencies that
    point to local folders via symlinks are copied as broken symlinks:
    https://github.com/electron/forge/issues/3238
    
    This commit fixes the following error on Ubuntu:
    
        > tracky-mouse-electron@2.2.0 make
        > electron-forge make
    
        ✔ Checking your system
        ✔ Loading configuration
        ✔ Resolving make targets
          › Making for the following targets: ,
        ✔ Running package command
          ✔ Preparing to package application
          ✔ Running packaging hooks
            ✔ Running generateAssets hook
            ✔ Running prePackage hook
          ✔ Packaging application
            ✔ Packaging for x64 on linux [1m58s]
          ✔ Running postPackage hook
        ✔ Running preMake hook
        ❯ Making distributables
          ✖ Making a deb distributable for linux/x64
            › ENOENT: no such file or directory, stat '/home/io/Projects/tracky-mouse/desktop-app/out/Tracky
              Mouse-linux-x64/resources/app/node_modules/tracky-mouse'
          ✖ Making a rpm distributable for linux/x64
        ◼ Running postMake hook
    
        An unhandled rejection has occurred inside Forge:
        Error: ENOENT: no such file or directory, stat '/home/io/Projects/tracky-mouse/desktop-app/out/Tracky Mouse-linux-x64/resources/app/node_modules/tracky-mouse'
    
    With this in place, the build now succeeds on Ubuntu.
    https://github.com/1j01/tracky-mouse/issues/99

--- desktop-app/forge.config.js
+++ desktop-app/forge.config.js
@@ -34,6 +34,27 @@ module.exports = {
 		// TODO: maybe
 		// https://electron.github.io/packager/main/interfaces/Options.html#darwinDarkModeSupport
 	},
+	hooks: {
+		packageAfterPrune: async (_forgeConfig, buildPath) => {
+			// Fix broken symlinks in node_modules of the packaged app.
+			// This is needed on Ubuntu due to https://github.com/electron/forge/issues/3238
+			const { cp, rm } = require('fs').promises;
+			const { join, dirname } = require('path');
+			// TODO: find all symlinked modules instead of hardcoding
+			const modulesToCopy = ['tracky-mouse'];
+			for (const moduleName of modulesToCopy) {
+				// TODO: robustly find package folder
+				// This assumes package.json's main field points to a file at the root of the package.
+				// (There's also scoped packages to consider. Is there a proper way to do this?)
+				const moduleDir = dirname(require.resolve(moduleName));
+				const destPath = join(buildPath, 'node_modules', moduleName);
+				// Delete broken symlink
+				await rm(destPath, { recursive: true, force: true });
+				// Copy module folder
+				await cp(moduleDir, destPath, { recursive: true });
+			}
+		},
+	},
 	makers: [
 		{
 			name: "@electron-forge/maker-squirrel",
