1j01/tracky-mouse

commit e9871e00d3bdc368bac2989bfd16c034256c24b9

Work around Electron Forge bug with executableName
Apparently executableName has to match the package.json name field
when building for Linux.

I found this relevant issue, but it's labeled as a question and closed:
https://github.com/electron/forge/issues/709
There's also this one supposedly related to a vite plugin but open:
https://github.com/electron/forge/issues/3753

This commit fixes the following error on Ubuntu:

    > tracky-mouse-electron@2.2.0 make
    > electron-forge make

    ✔ Checking your system
    ✔ Loading configuration
    ✔ Resolving make targets
      › Making for the following targets: ,
    ✔ Running package command
      ✔ Preparing to package application
      ✔ Running packaging hooks
        ✔ Running generateAssets hook
        ✔ Running prePackage hook
      ✔ Packaging application
        ✔ Packaging for x64 on linux [53s]
      ✔ Running postPackage hook
    ✔ Running preMake hook
    ❯ Making distributables
      ✖ Making a deb distributable for linux/x64
      ✖ Making a rpm distributable for linux/x64
        › could not find the Electron app binary at "/home/io/Projects/tracky-mouse/desktop-app/out/Tracky
          Mouse-linux-x64/tracky-mouse-electron". You may need to re-bundle the app using Electron Packager's
          "executableName" option.
    ◼ Running postMake hook

    An unhandled rejection has occurred inside Forge:
    Error: could not find the Electron app binary at "/home/io/Projects/tracky-mouse/desktop-app/out/Tracky Mouse-linux-x64/tracky-mouse-electron". You may need to re-bundle the app using Electron Packager's "executableName" option.
    at /home/io/Projects/tracky-mouse/desktop-app/node_modules/electron-installer-common/src/installer.js:154:15
        at async Promise.all (index 2)
        at async module.exports (/home/io/Projects/tracky-mouse/desktop-app/node_modules/electron-installer-redhat/src/installer.js:168:3)

desktop-app/forge.config.js
@@ -19,7 +19,7 @@ module.exports = {
 	packagerConfig: {
 		icon: "./images/tracky-mouse-logo",
 		name: "Tracky Mouse",
-		executableName: "tracky-mouse",
+		executableName: "tracky-mouse-electron",
 		appBundleId: "io.isaiahodhner.tracky-mouse",
 		appCategoryType: "public.app-category.utilities",
 		appCopyright: "© 2024 Isaiah Odhner",

commit de6c96240a2bcb014c84191685bf125bc2f9fedd

Fix building on Windows
broken in e9871e00d3bdc368bac2989bfd16c034256c24b9

desktop-app/forge.config.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const sharedDebRpmOptions = {
 	name: "tracky-mouse",
 	productName: "Tracky Mouse",
@@ -19,7 +20,7 @@ module.exports = {
 	packagerConfig: {
 		icon: "./images/tracky-mouse-logo",
 		name: "Tracky Mouse",
-		executableName: "tracky-mouse-electron",
+		executableName: os.platform() === "linux" ? "tracky-mouse-electron" : "tracky-mouse",
 		appBundleId: "io.isaiahodhner.tracky-mouse",
 		appCategoryType: "public.app-category.utilities",
 		appCopyright: "© 2024 Isaiah Odhner",