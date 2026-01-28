144995a80dc7e419f642489927c01b9b642df530
Isaiah Odhner <isaiahodhner@gmail.com>
2026-01-27 19:20:33 -0500

    Fix update-cli-docs script CLI availability assumption

--- scripts/update-cli-docs.js
+++ scripts/update-cli-docs.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { execSync } = require('child_process');
 
 const cliMdPath = path.join(__dirname, '..', 'CLI.md');
-const cliHelpOutput = execSync('tracky-mouse --help').toString().trim();
+const cliHelpOutput = execSync('npx tracky-mouse --help').toString().trim();
 const oldMarkdown = fs.readFileSync(cliMdPath, 'utf8');
 const newMarkdown = oldMarkdown.replace(/```HELP_OUTPUT.+?```/s, '```HELP_OUTPUT\n' + cliHelpOutput + '\n```');
 fs.writeFileSync(cliMdPath, newMarkdown);
