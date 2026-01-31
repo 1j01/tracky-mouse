62665b75eb233f247c2f81ea62efdf3996db3ee9
Isaiah Odhner <isaiahodhner@gmail.com>
2026-01-31 00:39:13 -0500

    WIP: MSIX + Windows Store

--- desktop-app/forge.config.js
+++ desktop-app/forge.config.js
@@ -98,6 +98,18 @@ module.exports = {
 			},
 		},
 		{
+			name: '@electron-forge/maker-msix',
+			config: {
+				manifestVariables: {
+					publisher: 'Isaiah Odhner'
+				},
+				windowsSignOptions: {
+					// certificateFile: 'C:\\devcert.pfx',
+					// certificatePassword: '122345'
+				}
+			}
+		},
+		{
 			name: "@electron-forge/maker-zip",
 			platforms: [
 				"darwin",  // macOS uses a .zip, which may be automatically extracted when opened
