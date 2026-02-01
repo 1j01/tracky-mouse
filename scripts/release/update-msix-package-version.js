a3da1b6419725697df3407bb80b790374e875e8a
Isaiah Odhner <isaiahodhner@gmail.com>
2026-02-01 00:09:55 -0500

    //

--- scripts/release/update-msix-package-version.js
+++ scripts/release/update-msix-package-version.js
@@ -9,7 +9,8 @@ if (version !== require('../../package.json').version) {
 	console.error('VERSION env var does not match package.json version');
 	process.exit(1);
 }
-const msixVersion = version + '.0'; // MSIX version needs four parts
+// MSIX version needs four parts. The last part is reserved for the Windows Store to use, and must be zero.
+const msixVersion = version + '.0';
 const file = "desktop-app/Package.appxmanifest";
 const regex = /(<Identity\s*(?:Name="[^"]*"\s+)?Version=")([^"]*)(")/g;
 fs.writeFileSync(file, fs.readFileSync(file, 'utf8')
