52a12f3e692b4a55703a330eba93957b469d46d3
Isaiah Odhner <isaiahodhner@gmail.com>
2026-02-04 16:41:35 -0500

    Clean up Three.js template code

--- experiments/dancer/dancer.js
+++ experiments/dancer/dancer.js
@@ -3,7 +3,6 @@ import {
 	AmbientLight,
 	AxesHelper,
 	BoxGeometry,
-	Clock,
 	GridHelper,
 	Mesh,
 	MeshLambertMaterial,
@@ -16,31 +15,27 @@ import {
 	Scene,
 	WebGLRenderer
 } from 'three';
-// import { DragControls } from 'three/addons/controls/DragControls.js'
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-// import * as animations from './helpers/animations'
-// import { toggleFullScreen } from './helpers/fullscreen'
-// import { resizeRendererToDisplaySize } from './helpers/responsiveness'
-// import './style.css'
 
-
-let canvas; //: HTMLElement
-let renderer; //: WebGLRenderer
-let scene; //: Scene
-// let loadingManager; //: LoadingManager
-let ambientLight; //: AmbientLight
-let pointLight; //: PointLight
-let cube; //: Mesh
-let camera; //: PerspectiveCamera
-let cameraControls; //: OrbitControls
-// let dragControls; //: DragControls
-let axesHelper; //: AxesHelper
-let pointLightHelper; //: PointLightHelper
-let clock; //: Clock
-let stats; //: Stats
-// let gui; //: GUI
-
-const animation = { enabled: true, play: true };
+/** @type {HTMLCanvasElement} */
+let canvas;
+/** @type {WebGLRenderer} */
+let renderer;
+/** @type {Scene} */
+let scene;
+/** @type {AmbientLight} */
+let ambientLight;
+/** @type {PointLight} */
+let pointLight;
+/** @type {Mesh} */
+let cube;
+/** @type {PerspectiveCamera} */
+let camera;
+/** @type {AxesHelper} */
+let axesHelper;
+/** @type {PointLightHelper} */
+let pointLightHelper;
+/** @type {Stats} */
+let stats;
 
 init();
 animate();
@@ -56,25 +51,6 @@ function init() {
 		renderer.shadowMap.type = PCFSoftShadowMap;
 		scene = new Scene();
 	}
-
-	// ===== 👨🏻‍💼 LOADING MANAGER =====
-	// {
-	// 	loadingManager = new LoadingManager()
-
-	// 	loadingManager.onStart = () => {
-	// 		console.log('loading started')
-	// 	}
-	// 	loadingManager.onProgress = (url, loaded, total) => {
-	// 		console.log('loading in progress:')
-	// 		console.log(`${url} -> ${loaded} / ${total}`)
-	// 	}
-	// 	loadingManager.onLoad = () => {
-	// 		console.log('loaded!')
-	// 	}
-	// 	loadingManager.onError = () => {
-	// 		console.log('❌ error while loading')
-	// 	}
-	// }
 
 	// ===== 💡 LIGHTS =====
 	{
@@ -127,53 +103,6 @@ function init() {
 		camera.position.set(2, 2, 5);
 	}
 
-	// ===== 🕹️ CONTROLS =====
-	// {
-	// 	cameraControls = new OrbitControls(camera, canvas)
-	// 	cameraControls.target = cube.position.clone()
-	// 	cameraControls.enableDamping = true
-	// 	cameraControls.autoRotate = false
-	// 	cameraControls.update()
-
-	// 	dragControls = new DragControls([cube], camera, renderer.domElement)
-	// 	dragControls.addEventListener('hoveron', (event) => {
-	// 		const mesh = event.object as Mesh
-	// 		const material = mesh.material as MeshStandardMaterial
-	// 		material.emissive.set('green')
-	// 	})
-	// 	dragControls.addEventListener('hoveroff', (event) => {
-	// 		const mesh = event.object as Mesh
-	// 		const material = mesh.material as MeshStandardMaterial
-	// 		material.emissive.set('black')
-	// 	})
-	// 	dragControls.addEventListener('dragstart', (event) => {
-	// 		const mesh = event.object as Mesh
-	// 		const material = mesh.material as MeshStandardMaterial
-	// 		cameraControls.enabled = false
-	// 		animation.play = false
-	// 		material.emissive.set('orange')
-	// 		material.opacity = 0.7
-	// 		material.needsUpdate = true
-	// 	})
-	// 	dragControls.addEventListener('dragend', (event) => {
-	// 		cameraControls.enabled = true
-	// 		animation.play = true
-	// 		const mesh = event.object as Mesh
-	// 		const material = mesh.material as MeshStandardMaterial
-	// 		material.emissive.set('black')
-	// 		material.opacity = 1
-	// 		material.needsUpdate = true
-	// 	})
-	// 	dragControls.enabled = false
-
-	// 	// Full screen
-	// 	window.addEventListener('dblclick', (event) => {
-	// 		if (event.target === canvas) {
-	// 			toggleFullScreen(canvas)
-	// 		}
-	// 	})
-	// }
-
 	// ===== 🪄 HELPERS =====
 	{
 		axesHelper = new AxesHelper(4);
@@ -189,100 +118,23 @@ function init() {
 		scene.add(gridHelper);
 	}
 
-	// ===== 📈 STATS & CLOCK =====
+	// ===== 📈 STATS =====
 	{
-		clock = new Clock();
 		stats = new Stats();
 		document.body.appendChild(stats.dom);
 	}
-
-	// ==== 🐞 DEBUG GUI ====
-	// {
-	// 	gui = new GUI({ title: '🐞 Debug GUI', width: 300 })
-
-	// 	const cubeOneFolder = gui.addFolder('Cube one')
-
-	// 	cubeOneFolder.add(cube.position, 'x').min(-5).max(5).step(0.5).name('pos x')
-	// 	cubeOneFolder
-	// 		.add(cube.position, 'y')
-	// 		.min(-5)
-	// 		.max(5)
-	// 		.step(1)
-	// 		.name('pos y')
-	// 		.onChange(() => (animation.play = false))
-	// 		.onFinishChange(() => (animation.play = true))
-	// 	cubeOneFolder.add(cube.position, 'z').min(-5).max(5).step(0.5).name('pos z')
-
-	// 	cubeOneFolder.add(cube.material as MeshStandardMaterial, 'wireframe')
-	// 	cubeOneFolder.addColor(cube.material as MeshStandardMaterial, 'color')
-	// 	cubeOneFolder.add(cube.material as MeshStandardMaterial, 'metalness', 0, 1, 0.1)
-	// 	cubeOneFolder.add(cube.material as MeshStandardMaterial, 'roughness', 0, 1, 0.1)
-
-	// 	cubeOneFolder
-	// 		.add(cube.rotation, 'x', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
-	// 		.name('rotate x')
-	// 	cubeOneFolder
-	// 		.add(cube.rotation, 'y', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
-	// 		.name('rotate y')
-	// 		.onChange(() => (animation.play = false))
-	// 		.onFinishChange(() => (animation.play = true))
-	// 	cubeOneFolder
-	// 		.add(cube.rotation, 'z', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
-	// 		.name('rotate z')
-
-	// 	cubeOneFolder.add(animation, 'enabled').name('animated')
-
-	// 	const controlsFolder = gui.addFolder('Controls')
-	// 	controlsFolder.add(dragControls, 'enabled').name('drag controls')
-
-	// 	const lightsFolder = gui.addFolder('Lights')
-	// 	lightsFolder.add(pointLight, 'visible').name('point light')
-	// 	lightsFolder.add(ambientLight, 'visible').name('ambient light')
-
-	// 	const helpersFolder = gui.addFolder('Helpers')
-	// 	helpersFolder.add(axesHelper, 'visible').name('axes')
-	// 	helpersFolder.add(pointLightHelper, 'visible').name('pointLight')
-
-	// 	const cameraFolder = gui.addFolder('Camera')
-	// 	cameraFolder.add(cameraControls, 'autoRotate')
-
-	// 	// persist GUI state in local storage on changes
-	// 	gui.onFinishChange(() => {
-	// 		const guiState = gui.save()
-	// 		localStorage.setItem('guiState', JSON.stringify(guiState))
-	// 	})
-
-	// 	// load GUI state if available in local storage
-	// 	const guiState = localStorage.getItem('guiState')
-	// 	if (guiState) gui.load(JSON.parse(guiState))
-
-	// 	// reset GUI state button
-	// 	const resetGui = () => {
-	// 		localStorage.removeItem('guiState')
-	// 		gui.reset()
-	// 	}
-	// 	gui.add({ resetGui }, 'resetGui').name('RESET')
-
-	// 	gui.close()
-	// }
 }
 
 function animate() {
 	requestAnimationFrame(animate);
 
 	stats.begin();
-	// if (animation.enabled && animation.play) {
-	// 	animations.rotate(cube, clock, Math.PI / 3);
-	// 	animations.bounce(cube, clock, 1, 0.5, 0.5);
-	// }
 
 	if (resizeRendererToDisplaySize(renderer)) {
 		const canvas = renderer.domElement;
 		camera.aspect = canvas.clientWidth / canvas.clientHeight;
 		camera.updateProjectionMatrix();
 	}
-
-	// cameraControls.update();
 
 	renderer.render(scene, camera);
 	stats.end();
