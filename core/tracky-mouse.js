1j01/tracky-mouse

commit 6d63967166ad104d4cf36f0ee9e0126a7f61c211

Fully parallelize loading now that tf dependency is gone

core/tracky-mouse.js
@@ -26,19 +26,12 @@ TrackyMouse.loadDependencies = function ({ statsJs = false } = {}) {
 		`${TrackyMouse.dependenciesRoot}/lib/no-eval.js`, // generated with eval-is-evil.html, this instruments clmtrackr.js so I don't need unsafe-eval in the CSP
 		`${TrackyMouse.dependenciesRoot}/lib/clmtrackr.js`,
 		`${TrackyMouse.dependenciesRoot}/lib/face_mesh/face_mesh.js`,
-		// `${TrackyMouse.dependenciesRoot}/lib/tf-core.min.js`,
-	];
-	const moreScriptFiles = [
-		// These depend on tf-core.min.js
-		// `${TrackyMouse.dependenciesRoot}/lib/tf-backend-webgl.min.js`,
 		`${TrackyMouse.dependenciesRoot}/lib/face-landmarks-detection.min.js`,
 	];
 	if (statsJs) {
 		scriptFiles.push(`${TrackyMouse.dependenciesRoot}/lib/stats.js`);
 	}
-	return Promise.all(scriptFiles.map(loadScript)).then(() => {
-		return Promise.all(moreScriptFiles.map(loadScript));
-	});
+	return Promise.all(scriptFiles.map(loadScript));
 };
 
 const is_selector_valid = ((dummy_element) =>

commit 307e2b98196f6e5c26ad3e90707024883155dcbd

Fix "TypeError: o.Facemesh is not a constructor"
I think this was a race condition introduced when simplifying loading
in https://github.com/1j01/tracky-mouse/commit/6d63967166ad104d4cf36f0ee9e0126a7f61c211

Fixes https://github.com/1j01/tracky-mouse/issues/113

core/tracky-mouse.js
@@ -27,13 +27,20 @@ TrackyMouse.loadDependencies = function ({ statsJs = false } = {}) {
 		`${TrackyMouse.dependenciesRoot}/lib/no-eval.js`, // generated with eval-is-evil.html, this instruments clmtrackr.js so I don't need unsafe-eval in the CSP
 		`${TrackyMouse.dependenciesRoot}/lib/clmtrackr.js`,
 		`${TrackyMouse.dependenciesRoot}/lib/face_mesh/face_mesh.js`,
-		`${TrackyMouse.dependenciesRoot}/lib/face-landmarks-detection.min.js`,
 		`${TrackyMouse.dependenciesRoot}/lib/OneEuroFilter.js`,
+	];
+	// face-landmarks-detection.min.js depends on face_mesh.js
+	// avoid sporadic "TypeError: o.Facemesh is not a constructor" by loading face-landmarks-detection after face_mesh.js
+	// TODO: preload in parallel?
+	const moreScriptFiles = [
+		`${TrackyMouse.dependenciesRoot}/lib/face-landmarks-detection.min.js`,
 	];
 	if (statsJs) {
 		scriptFiles.push(`${TrackyMouse.dependenciesRoot}/lib/stats.js`);
 	}
-	return Promise.all(scriptFiles.map(loadScript));
+	return Promise.all(scriptFiles.map(loadScript)).then(() => {
+		return Promise.all(moreScriptFiles.map(loadScript));
+	});
 };
 
 const isSelectorValid = ((dummyElement) =>