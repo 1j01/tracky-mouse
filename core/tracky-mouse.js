0e81eebfb1c498d6fd4ff4454069f275bf193bb3
Isaiah Odhner <isaiahodhner@gmail.com>
2026-08-26 23:05:17 -0400

    In script tag shim, load API if loadDependencies is used

--- core/tracky-mouse.js
+++ core/tracky-mouse.js
@@ -1,14 +1,38 @@
 
+// This file provides a partial shim for the old script tag way of loading TrackyMouse.
+// It only works if you await `TrackyMouse.loadDependencies()` before using any other APIs,
+// which was not normally required for all APIs (nor is it required for all APIs when importing TrackyMouse as a module).
+
 // eslint-disable-next-line no-unused-vars
 const TrackyMouse = (() => {
 	const deprecationMessage = "Loading TrackyMouse as a global variable via script tag is no longer supported. Please import TrackyMouse from the \"tracky-mouse\" module instead.";
+	console.warn(deprecationMessage);
+
+	let tmAPI = null;
+	let propertiesBeforeLoad = {};
 
 	return new Proxy({}, {
-		get(_target, _prop) {
+		get(_target, prop) {
+			if (tmAPI) {
+				return tmAPI[prop];
+			} else if (Object.hasOwn(propertiesBeforeLoad, prop)) {
+				return propertiesBeforeLoad[prop];
+			} else if (prop === "loadDependencies") {
+				return async (...args) => {
+					tmAPI = (await import("./src/tracky-mouse.js")).TrackyMouse;
+					Object.assign(tmAPI, propertiesBeforeLoad);
+					return tmAPI.loadDependencies(...args);
+				};
+			}
 			throw new Error(deprecationMessage);
 		},
-		set(_target, _prop, _value) {
-			throw new Error(deprecationMessage);
+		set(_target, prop, value) {
+			if (tmAPI) {
+				tmAPI[prop] = value;
+				return true;
+			}
+			propertiesBeforeLoad[prop] = value;
+			return true;
 		},
 	});
 })();
